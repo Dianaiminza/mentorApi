7317f63524e66016319705990c091572698b5c20
Captain <diana.iminza@ronforddigital.com>
2024-07-23 09:28:38 +0300

    add nodemailer

--- configuration.js
+++ configuration.js
@@ -5,6 +5,7 @@ dotenv.config();
 module.exports={
 PORT: process.env.PORT || 5000,
 JWT_SECRET: process.env.JWT_SECRET || 'Zqc7mJMrGdEiIk&zF2C4K03Chk3TXwKH',
-BASE_URL:process.env.BASE_URL || 'https://mentorapi-tawz.onrender.com'
-
+BASE_URL:process.env.BASE_URL || 'https://mentorapi-tawz.onrender.com',
+Email:process.env.EMAIL ||'dianaiminza.99@gmail.com',
+Password:process.env.EMAIL_PASSWORD || ''
 };
